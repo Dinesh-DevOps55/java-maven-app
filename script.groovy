23ac7f0ded026be87e7c73b43eb4d3ac2294537e
Dinesh <157553324+Dinesh-DevOps55@users.noreply.github.com>
2025-07-01 10:39:40 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,5 +1,13 @@
-def deployApp() {
-    echo 'deploying the application...'
-}
+def buildJar() {
+    echo 'building the application...'
+    sh 'mvn package'
+}Add commentMore actions
 
-return this
+def buildImage() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t nanatwn/demo-app:jma-2.0 .'
+        sh 'echo $PASS | docker login -u $USER --password-stdin'
+        sh 'docker push nanatwn/demo-app:jma-2.0'
+    }
+}
